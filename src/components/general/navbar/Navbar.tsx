be660acbff6ad6fad967ca43c81a281002dec34c
Eryk-Kanownik <eryk.patryk.kanownik@gmail.com>
2024-09-04 16:02:52 +0200

    nav correction

--- src/components/general/navbar/Navbar.tsx
+++ src/components/general/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavbarLogo from "./NavbarLogo";
 import NavbarListItem from "./NavbarListItem";
 import { FaBars } from "react-icons/fa";
@@ -14,14 +14,14 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`text-red-500 ${
+      className={`text-red-500 bg-black ${
         isMenuUnfolded ? "bg-black" : ""
-      } pl-4 py-8 pr-8 lg:p-8 flex flex-col md:flex-row md:justify-between fixed top-0 left-0 right-0 z-20`}>
+      } pl-4 py-4 pr-8 lg:p-4 flex flex-col md:flex-row md:justify-between md:items-center fixed top-0 left-0 right-0 z-20`}>
       <NavbarLogo />
       <ul
-        className={`flex flex-col mt-6 gap-5 ${
-          isMenuUnfolded ? "scale-y-100" : "scale-y-0"
-        } md:scale-y-100 md:flex-row md:items-center md:mt-0 duration-200 origin-top`}>
+        className={`flex  flex-col  gap-5 overflow-hidden ${
+          isMenuUnfolded ? "max-h-[600px] mt-6" : "max-h-0 mt-0"
+        } md:max-h-[600px] md:flex-row md:items-center md:mt-0 origin-top`}>
         <NavbarListItem
           path="/products"
           text="Produkty"
@@ -48,7 +48,7 @@ const Navbar = () => {
           onClick={onClickNavbarListItem}
         />
       </ul>
-      <div className="md:hidden absolute top-8 right-8">
+      <div className="md:hidden absolute top-5 right-5">
         <FaBars
           className={`cursor-pointer ${isMenuUnfolded ? "text-red-600" : ""}`}
           onClick={() => setIsMenuUnfolded((prev) => !prev)}
