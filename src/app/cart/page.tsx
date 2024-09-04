c1a74c86e33148cccb2958a7ac2067c21d4582ad
Eryk-Kanownik <eryk.patryk.kanownik@gmail.com>
2024-09-04 20:27:54 +0200

    title and description

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
   return (
     <div className="px-4 lg:px-[20%] py-[90px] lg:py-32">
       <h1
-        className={`${diploma.className} text-4xl mb-6 font-semibold text-red-700`}>
+        className={`${diploma.className} text-4xl mb-2 font-semibold text-red-700`}>
         Koszyk
       </h1>
       <CartObjects />
--- src/components/sections/cart/CartObjects.tsx
+++ src/components/sections/cart/CartObjects.tsx
@@ -5,8 +5,18 @@ import React from "react";
 
 const CartObjects = () => {
   const { cart } = useCartStore<IUseCartStore>((state) => state);
+
   return (
     <div className="flex flex-col gap-4">
+      <h3 className="mb-2 text-xl font-semibold">
+        Ilość przedmiotów:{" "}
+        {cart.length > 0
+          ? cart
+              .map((item: any) => item.count)
+              .reduce((total, val) => total + val)
+          : 0}
+      </h3>
+
       {cart?.map(
         (
           { product: { id, name, category, price, size }, count }: any,
